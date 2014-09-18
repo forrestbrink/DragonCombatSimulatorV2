f979baf2af51ede13efc58c2de4ded1dc540b824
SeedPaths-User <lessdismissive@email.com>
2014-09-18 10:40:28 -0600

    complete

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DragonCombatSimulatorV2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //call the game
+            Game game = new Game();
+
+            game.PlayGame();
+            //keep the console open
+            Console.ReadKey();
+        }
+    }
+}
